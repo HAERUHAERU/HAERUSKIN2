138e7242513d78867bcb8c140a1471e24417ee66
HAERUHAERU <ffxivhaeru@gmail.com>
2019-12-09 17:54:43 +0900

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -4,8 +4,8 @@ lang = {
     BM: '',
     edit: '',    
     data: {
-        kr: '[191022] 해루스킨 2 - 모그리/해루',
-        en: '[191022] HAERUSKIN 2 - KR Moogle/HAERU(해루)'
+        kr: '[191209] 해루스킨 2 - 모그리/해루',
+        en: '[191209] HAERUSKIN 2 - KR Moogle/HAERU(해루)'
     },
     size: {
         1: '55.5%',
